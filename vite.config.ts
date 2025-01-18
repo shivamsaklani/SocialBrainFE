669ecb3e19ea264bd757d5a5a731fb826ca608e7
shivam <shivasaklani1@gmail.com>
2025-01-18 06:43:13 +0000

    build resolved

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './dist', // Ensures output is saved in ./dist
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
  
 })
